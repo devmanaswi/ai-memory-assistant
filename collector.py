5765cd92c681a632e78e0c1ef01c3bdbcbd8e117
Golla Manaswi <manaswigolla250@gmail.com>
2026-02-26 20:56:34 +0530

    Added timestamp and UTF-8 encoding support

---
+++ collector.py
@@ -0,0 +1,20 @@
+# Open conversation file in append mode
+
+# Start loop
+
+# Take user input
+
+# If input is DONE -> break loop
+
+# Else -> write input to file
+
+# Close file
+from datetime import datetime
+
+with open("conversation.txt", "a",encoding="utf-8") as f:
+    print(f"----------NEW SESSION  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}----------")
+    while True:
+        user_input = input("You: ")
+        if (user_input.strip()).lower() == "done":
+            break            
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +" USER: "+ user_input + "\n")
