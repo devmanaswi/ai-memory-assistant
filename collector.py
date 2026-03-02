fd2e0931b0c4485196c19da27448f99ef0822610
Golla Manaswi <manaswigolla250@gmail.com>
2026-03-03 04:48:43 +0530

    Refactored collector with modular structure

--- collector.py
+++ collector.py
@@ -1,4 +1,5 @@
-
+# AI Memory Assistant Project
+# git status# Open conversation file in append mode
 
 # Start loop
 
@@ -11,10 +12,26 @@
 # Close file
 from datetime import datetime
 
-with open("conversation.txt", "a",encoding="utf-8") as f:
-    print(f"----------NEW SESSION  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}----------")
-    while True:
-        user_input = input("You: ")
-        if (user_input.strip()).lower() == "done":
-            break            
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +" USER: "+ user_input + "\n")
+def start_session():
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f"\n---------- NEW SESSION {timestamp} ----------\n"
+
+def save_message(file, role, message):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    file.write(f"{timestamp} {role}: {message}\n")
+
+def main():
+    with open("conversation.txt", "a", encoding="utf-8") as f:
+        f.write(start_session())
+
+        while True:
+            user_input = input("You: ")
+
+            if user_input.strip().lower() == "done":
+                print("Session ended.")
+                break
+
+            save_message(f, "USER", user_input)
+
+if __name__ == "__main__":
+    main()
